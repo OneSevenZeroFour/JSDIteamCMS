7e40b7b55ccdc31406f1e9631082a4c3935a2e83
Liu Yun <1601047350@qq.com>
2017-09-23 15:13:12 +0800

    整合处理

--- src/node/server.js
+++ src/node/server.js
@@ -304,14 +304,12 @@ io.on('connection',function(socket){
             });
         }
         user.forEach(function(item){
-            if(item.name == 'admin'){
-                if(item.id){
-                    io.sockets.sockets[item.id].emit('id',{
-                        name:data.name,
-                        id:socket.id,
-                        value:data.value,
-                    });   
-                }   
+            if(item.name == 'admin'){  
+                io.sockets.sockets[item.id].emit('id',{
+                    name:data.name,
+                    id:socket.id,
+                    value:data.value,
+                });                  
             }
             
         });
